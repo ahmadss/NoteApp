5f03ef5f080247c04c35e7a362428e58a69fbd2f
Didiet <mobile.developer@altama.co.id>
2021-10-12 13:34:39 +0700

    -- Initial project

---
+++ app/src/main/java/id/co/example/noteapp/note_feature/domain/model/Note.kt
@@ -0,0 +1,19 @@
+package id.co.example.noteapp.note_feature.domain.model
+
+import androidx.compose.ui.graphics.Color
+import androidx.room.PrimaryKey
+import id.co.example.noteapp.ui.theme.Purple200
+import id.co.example.noteapp.ui.theme.Purple700
+import id.co.example.noteapp.ui.theme.Teal200
+
+data class Note(
+    val title : String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val Id : Int? = null
+) {
+    companion object{
+        val noteColorsList = listOf(Purple200, Color.Red, Color.Green, Color.Blue, Color.LightGray, Teal200)
+    }
+}
